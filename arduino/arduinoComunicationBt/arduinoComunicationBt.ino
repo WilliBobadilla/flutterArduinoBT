de5e6a6005996b3538848990d8acaff44350b0cc
WilliBobadilla <willi1997.1@gmail.com>
2021-03-27 13:29:26 -0300

    updating arduino code

--- arduino/arduinoComunicationBt/arduinoComunicationBt.ino
+++ arduino/arduinoComunicationBt/arduinoComunicationBt.ino
@@ -6,21 +6,39 @@
 */
 
 #include <SoftwareSerial.h>
+#include <Servo.h> 
 
-
-
+Servo servo;  //creamos un objeto servo 
 SoftwareSerial BT(7, 8); // RX, TX
 String received = "";
 bool receiving = false;
 
 
+
+long tiempoActual;//para guardar el tiempo en todo momento
+long tiempoPrevio=0;
+const long intervalo=0;
+int lecturaSensor;
+
 void setup() {
   Serial.begin(9600);
   BT.begin(9600);
+  servo.attach(4);//indicamos que esta en la patita 4 
+  servo.write(0);//comenzamos en el angulo 0
 }
 
 
 void loop() {
+
+
+ if(tiempoActual-tiempoPrevio>=intervalo ){
+   tiempoPrevio=tiempoActual;//actualizamos el tiempo previo
+   lecturaSensor=analogRead(A0);
+   escribir_datos(lecturaSensor);
+ }
+
+
+
   while (BT.available() > 0) {
     char data = BT.read();
     received = received + data;
@@ -32,12 +50,13 @@ void loop() {
     //despues de salir del while es que recien podemos tener el mensaje completo
     Serial.print("recibido:  ");
     Serial.println(received);
+    servo.write(received.toInt());
     receiving = false;
     received = ""; //limpiamos para futuros mensajes
   }
 }
 
-void escribir_datos(String dataToWrite) {
+void escribir_datos(int dataToWrite) {
   BT.print("#");
   BT.print(dataToWrite);
   BT.println("*");
